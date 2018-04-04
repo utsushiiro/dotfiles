695b65dccf323dbccb843c86e6ee8dba14e93f61
utsushiiro <ozeki.haru@gmail.com>
2018-04-04 12:56:37 +0900

    install.shにNeobundleのインストールを追加

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,5 @@
 wget "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh" --no-clobber -P $HOME/dotfiles/etc/downloads
 wget "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" --no-clobber -P $HOME/dotfiles/etc/downloads
-
-exec $HOME/dotfiles/bin/setlink.sh
+mkdir -p $HOME/.vim/bundle
+git clone git://github.com/Shougo/neobundle.vim $HOME/.vim/bundle/neobundle.vim
+$HOME/dotfiles/bin/setlink.sh
